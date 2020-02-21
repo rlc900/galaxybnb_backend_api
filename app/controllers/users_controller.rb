3d3e465866b751ed0686c55278ad09b6eabd005b
Rianna Cleary <rianna.cleary@gmail.com>
2020-02-21 17:51:23 -0500

    second changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+
+    render json: @users
+  end
+
+  def login
+    # byebug
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      render json: @user
+    else
+      render json: {error: 'Invalid username or password'}
+    end
+  end
+
+
+private
+
+def user_params
+  params.permit(:username, :password)
+end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   resources :reviews
   resources :types
   resources :users
+
+  # CUSTOM ROUTES
+
+  post '/login', to: "users#login"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
