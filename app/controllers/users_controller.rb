e474eac809883e031e6f2b0e1c4f083017bc1205
Rianna Cleary <rianna.cleary@gmail.com>
2020-02-23 18:17:37 -0500

    jwt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authorized, only: [:persist]
+
 
   def index
     @users = User.all
@@ -19,18 +21,24 @@ class UsersController < ApplicationController
     # byebug
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
-      render json: @user
+      token = encode_token({user_id: @user.id})
+      render json: {user: UserSerializer.new(@user), token: token}
     else
       render json: {error: 'Invalid username or password'}
     end
   end
 
+  def persist
+    token = encode_token({user_id: @user.id})
+    render json: {user: UserSerializer.new(@user), token: token}
+  end
+
 
 private
 
-def user_params
-  params.permit(:username, :password)
-end
+  def user_params
+    params.permit(:username, :password)
+  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
 
   post '/login', to: "users#login"
   post '/signup', to: 'users#create'
+  get '/persist', to: 'users#persist'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
