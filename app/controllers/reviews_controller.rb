79886944af61f550d497077181cffc4c3e8d727e
Rianna Cleary <rianna.cleary@gmail.com>
2020-03-01 13:34:41 -0500

    fixed some methods for reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,19 @@
 class ReviewsController < ApplicationController
+
   def index
     @reviews = Review.all
 
     render json: @reviews
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def create
+    @user = User.find(params[:id])
+    @review = Review.create(rating: params[:rating], user_id: @user.id)
+
+    render json: @review
   end
 end
