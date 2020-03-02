38ce5753f330796634f1e0dccbd66974dfe8c123
Rianna Cleary <rianna.cleary@gmail.com>
2020-03-02 11:35:24 -0500

    made reviews attribute of location in serializer

--- app/serializers/location_serializer.rb
+++ app/serializers/location_serializer.rb
@@ -1,7 +1,7 @@
 class LocationSerializer < ActiveModel::Serializer
-  attributes :id, :name, :image
+  attributes :id, :name, :image, :reviews
 
-  has_many :reviews
+  # has_many :reviews
 end
 
 # changed line 4
--- app/serializers/planet_serializer.rb
+++ app/serializers/planet_serializer.rb
@@ -2,4 +2,10 @@ class PlanetSerializer < ActiveModel::Serializer
   attributes :id, :name, :climate, :terrain, :image
 
   has_many :locations
+
+  # def getReviews
+  #   # byebug
+  #   self.object.locations.map {|location| location.reviews}
+  # end
+
 end
