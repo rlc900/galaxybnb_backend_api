ef9df2fb2ecf8f60a106ea7f5aee3796b8f5142a
Rianna Cleary <rianna.cleary@gmail.com>
2020-03-04 18:22:35 -0500

    stripe workd!

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,8 @@ Rails.application.routes.draw do
 
   post '/login', to: "users#login"
   post '/signup', to: 'users#create'
-  get '/persist', to: 'users#persist'
   post '/charges', to: 'charges#create'
-
+  get '/persist', to: 'users#persist'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
