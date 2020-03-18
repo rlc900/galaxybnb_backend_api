9c32ada143c65ba1976cae3f3b8007c06ad55236
Rianna Cleary <rianna.cleary@gmail.com>
2020-03-18 17:43:16 -0400

    added ams file

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,10 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :location
+
+  def username
+    review_user = self.user_id
+    found_user = User.find_by(id: review_user)
+    found_user.username
+  end
 end
--- app/serializers/planet_serializer.rb
+++ app/serializers/planet_serializer.rb
@@ -1,5 +1,5 @@
 class PlanetSerializer < ActiveModel::Serializer
-  attributes :id, :name, :climate, :terrain, :images
+  attributes :id, :name, :climate, :terrain, :image
 
   has_many :locations
 
